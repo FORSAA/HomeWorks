9b19e75c141ffba962ab0e0804b5244fc79851b4
FORSAA <118635418+FORSAA@users.noreply.github.com>
2022-12-07 20:31:14 +0300

    Modified BaseAviaryClass.py

--- ClassesUpdated(By Egor)/BaseAviaryClass.py
+++ ClassesUpdated(By Egor)/BaseAviaryClass.py
@@ -1,10 +1,13 @@
 
 class BaseAviary:
 
-    def __init__(self, name):
+    def __init__(self, name, canContainPredator=False):
         self._aviaryName = name
         self._aviaryBiom = "   "
         self._aviarySquare = "   "
+        self._animalsInAviary = []
+        self._maxAnimalsInAviary = 0
+        self._canContainPredator = canContainPredator
 
     """GETTERS"""
     @property
@@ -19,7 +22,23 @@ class BaseAviary:
     def aviarySquare(self):
         return self._aviarySquare
 
+    @property
+    def animalsInAviary(self):
+        return self._animalsInAviary
+
+    @property
+    def maxAnimalsInAviary(self):
+        return self._maxAnimalsInAviary
+
+    @maxAnimalsInAviary.setter
+    def maxAnimalsInAviary(self, maxAnimals):
+        if(maxAnimals>=0 and maxAnimals<5):
+            self._maxAnimalsInAviary = maxAnimals
+
 
     def AddAnimalToAviary(self, animalType):
-        print(animalType.mass)
-        pass
+        if True:
+            if(self._canContainPredator == False and animalType.isPredator == False and animalType.liveSquare <= self._aviarySquare and animalType.biom == self._aviaryBiom):
+                self._animalsInAviary.append(animalType)
+            elif (self._canContainPredator == True and animalType.isPredator == True and animalType.liveSquare <= self._aviarySquare and animalType.biom == self._aviaryBiom):
+                self._animalsInAviary.append(animalType)
